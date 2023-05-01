02d7be5381709ca1d5a13dee0f4fee1eca21b93f
chawkimbarki <chawkimbarki500@gmail.com>
2023-05-01 14:24:30 +0100

    Create a linked list to store books

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+class node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
+class linkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+}
+
+let books = new linkedList()
