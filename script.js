b53dc554c64832a05a9b2d4ca2872ee867669fe8
chawkimbarki <chawkimbarki500@gmail.com>
2023-05-02 11:07:21 +0100

    Add grabbing books Function and final touches

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ class book {
 }
 
 class linkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
+    constructor(head, tail, size) {
+        this.head = head;
+        this.tail = tail;
+        this.size = size;
     }
 
     add(value) {
@@ -29,6 +29,9 @@ class linkedList {
             this.tail = newNode;
         }
         this.size += 1;
+        let boks = JSON.stringify(this);
+        localStorage.removeItem('books');
+        localStorage.setItem('books', boks);
     }
 
     remove(index) {
@@ -37,21 +40,27 @@ class linkedList {
             this.head = null;
             this.tail = null;
             this.size = 0;
-        } else if (index === 0) {
-            this.head = current.next;
-            this.size -= 1;
+            localStorage.removeItem('books');
         } else {
-            let previous;
-            for (let i = 1; i <= index; i += 1) {
-                previous = current;
-                current = current.next;
+            if (index === 0) {
+                this.head = current.next;
+            } else {
+                let previous;
+                for (let i = 1; i <= index; i += 1) {
+                    previous = current;
+                    current = current.next;
+                }
+                if (index === this.size - 1) {
+                    this.tail = previous;
+                }
+                previous.next = current.next;
             }
-            if (index === this.size - 1) {
-                this.tail = previous;
-            }
-            previous.next = current.next;
+            let boks = JSON.stringify(this);
+            localStorage.removeItem('books');
+            localStorage.setItem('books', boks);
+            this.size -= 1;
         }
-        displayAllBooks(books);
+        displayAllBooks(this);
     }
 
     // this is for testing
@@ -98,8 +107,23 @@ function displayBook(book, index) {
     booksContainer.innerHTML += newBook;
 }
 
-const body = document.body;
-const books = new linkedList();
+
+
+function grabbingBooks() {
+    let books = JSON.parse(localStorage.getItem('books'));
+    if (books === null) {
+        books = new linkedList(null, null, 0);
+    } else {
+        books = new linkedList(books.head, books.tail, books.size);
+        const tail = books.tail;
+        books.remove(books.size - 1);
+        books.add(tail.value);
+        displayAllBooks(books);
+    }
+    return books;
+}
+
+const books = grabbingBooks();
 const btn = document.getElementById("add");
 
 btn.addEventListener("click", function () {
